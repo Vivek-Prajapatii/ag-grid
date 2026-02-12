b17feaea44ffd48c6a145149aff0ee145b9b5c6d
Vivek-Prajapatii <prajapativivek415@gmail.com>
2026-02-12 18:55:47 +0530

    added ag-grid and made fields editable

---
+++ src/components/AgGrid.js
@@ -0,0 +1,41 @@
+import { AllCommunityModule } from "ag-grid-community";
+import { AgGridProvider, AgGridReact } from "ag-grid-react";
+import { useMemo, useState } from "react";
+import { employeeColumns, employeeData } from "../../employee.config";
+
+export default function AgGrid() {
+  // Column Definitions
+  const [colDefs] = useState(employeeColumns);
+
+  // Row Data
+  const [rowData] = useState(employeeData?.employees);
+  const modules = [AllCommunityModule];
+
+  const defaultColDef = useMemo(() => {
+    return {
+      filter: "agTextColumnFilter",
+      floatingFilter: true,
+    };
+  }, []);
+
+  const rowSelection = {
+    mode: "multiRow",
+    headerCheckbox: true,
+  };
+
+  return (
+    <AgGridProvider modules={modules}>
+      <div style={{ height: "650px", width: "100%" }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          defaultColDef={defaultColDef}
+          pagination={true}
+          paginationPageSize={15}
+          paginationPageSizeSelector={[10, 15, 20]}
+          rowSelection={rowSelection}
+        />
+      </div>
+    </AgGridProvider>
+  );
+}
